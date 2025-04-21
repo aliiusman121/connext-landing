cb5ab77d8110fc2eba2d4065b2a695b95e4c7684
aliiusman121 <aliusman.mah@gmail.com>
2025-04-21 10:58:41 -0400

    fix: router working for signup navigation

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
-import Signup from "./Signup";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import Signup from "./Signup";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom'; // 🧙 magic portal
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Router>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/signup" element={<Signup />} />
-    </Routes>
-  </Router>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
+
