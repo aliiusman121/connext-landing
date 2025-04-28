8831d1472d586dcf67ccc0c5eb95456de5ac8e21
aliiusman121 <aliusman.mah@gmail.com>
2025-04-28 11:54:43 -0400

    feat: add working signup form connected to backend

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './Home';
+import Signup from "./Signup";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<Signup />} />
       </Routes>
     </BrowserRouter>
   );
